8adff338cb6132fcf053aea1b6b53933603eb99f
hubbingingit <120624468+harshinimm@users.noreply.github.com>
2025-04-19 00:12:40 +0800

    Update User.java

--- User.java
+++ User.java
@@ -1,38 +1,40 @@
-public abstract class User {
+public class User {
+
     protected String name;
     protected String nric;
-    protected String password;
-    protected int age;
-    protected String maritalStatus;
+    private String password;
+    private int age;
+    private String maritalStatus;
 
-    public User(String name, String nric, String password, int age, String maritalStatus) {
-        this.name = name;
-        this.nric = nric;
-        this.password = password;
-        this.age = age;
-        this.maritalStatus = maritalStatus;
+    public User(String name, String nric, int age, String maritalStatus, String password) {
     }
+
+    public static void login(String NRIC,String password){
+
+
+     }
 
     public String getName() {
         return name;
     }
-    public String getNric() {
+
+    public String getUserId() {
         return nric;
     }
-    public String getPassword() {
+    public String getPassword(){
         return password;
     }
+
     public int getAge() {
         return age;
     }
+
     public String getMaritalStatus() {
         return maritalStatus;
     }
 
-    public void setPassword(String password) {
+    protected void setPassword(String password) {
         this.password = password;
     }
-    public void setMaritalStatus(String maritalStatus) {
-        this.maritalStatus = maritalStatus;
-    }
+
 }
